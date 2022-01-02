74286f455a508823035644ded1efa1717c45ef4d
Camilo Hernández <chernandez@quimera.dev>
2022-01-02 15:43:48 -0300

    Fixed unicode bug with the version

--- src/main.cpp
+++ src/main.cpp
@@ -12,18 +12,18 @@ int main(int argc, char **argv) {
     tiny::Logger *logger = &tiny::Logger::getInstance();
     logger->setLevel(tiny::LogLv::Debug);
 
-    if (argc == 2 && (strcmp(argv[1], "--version") == 0 || strcmp(argv[1], "version") == 0)) {
-        std::cout << TINY_NAME << " " << TINY_VERSION << " (" << TINY_VERSION_NICKNAME << "). "
-                  << TINY_COPYRIGHT << " " << TINY_LICENCE << std::endl;
-        return 0;
-    }
-
     try {
         std::locale::global(std::locale("en_US.UTF8"));
     } catch (std::runtime_error const &) {
         logger->warning(
                 "Unable to use 'en_US.UTF8' locale. Using the system's default ('" + std::locale().name() + "').");
         logger->warning("Non-ASCII characters might be unrecognized.");
+    }
+
+    if (argc == 2 && (strcmp(argv[1], "--version") == 0 || strcmp(argv[1], "version") == 0)) {
+        std::cout << TINY_NAME << " " << TINY_VERSION << " (" << TINY_VERSION_NICKNAME << "). "
+                  << TINY_COPYRIGHT << " " << TINY_LICENCE << std::endl;
+        return 0;
     }
 
     tiny::Compiler compiler;
